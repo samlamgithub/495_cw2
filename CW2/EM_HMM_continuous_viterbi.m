5da4c472b0ef81f4ffeaa7353ee5a5da0fdb0b5e
Jiahao Lin <jiahao.lin13@ic.ac.uk>
2017-02-28 01:29:10 +0000

    fix vertibi

--- CW2/EM_HMM_continuous_viterbi.m
+++ CW2/EM_HMM_continuous_viterbi.m
@@ -14,17 +14,22 @@ for n = 1:N
         delta_probs(1,k) = logPi(k) + log2(normpdf(Y(n, 1), E_e.mu(k), sqrt(E_e.sigma2(k))));
     end
     % init first latent
-    [nothing, S_e(n,1)] = max(delta_probs(1, :));
+    Z(1, :) = [1, 2];
     
     for t = 2:T
          for k = 1:K
                % pro of end up in latent j at time t: from latent i at time t-1
                % delta(t, j) = C_t^-1 x p(x_t|ztj=1) max for i: delta(t-1,
                % i) x A(i to j)
-             delta_probs(t, k) = log2(normpdf(Y(n, t), E_e.mu(k), sqrt(E_e.sigma2(k)))) + max(delta_probs(t-1, :)' + logA(:, k));
+             [maxP, maxZ] = max(delta_probs(t-1, :)' + logA(:, k))
+             Z(t, k) = maxZ;
+             delta_probs(t, k) = log2(normpdf(Y(n, t), E_e.mu(k), sqrt(E_e.sigma2(k)))) + maxP;
          end
-         % keep track of max porob latent
-         [nothing, S_e(n, t)] = max(delta_probs(t, :));
-    end 
+    end
+    % backtrack
+    [nothing, S_e(n, T)] = max(delta_probs(T, :));
+    for t=T-1:-1:1
+          S_e(n, t) = Z(t+1, S_e(n, t+1));
+    end
 end
 end
--- CW2/EM_HMM_discrete_viterbi.m
+++ CW2/EM_HMM_discrete_viterbi.m
@@ -10,18 +10,25 @@ logE = log2(E_e);
 
 for n = 1:N
     delta_probs = zeros(T, K);
+    Z = zeros(T, K);
     delta_probs(1, :) = logPi' + logE(:, Y(n, 1))'; % init the first observation prob
-    [nothing, S_e(n,1)] = max(delta_probs(1, :)); % init first latent
+    % [nothing, Z(n,1)] = max(delta_probs(1, :)); % init first latent
+    Z(1, :) = [1, 2];
     for t = 2:T
          for k = 1:K
                % pro of end up in latent j at time t: from latent i at time t-1
                % delta(t, j) = C_t^-1 x p(x_t|ztj=1) max for i: delta(t-1,
                % i) x A(i to j)
-             delta_probs(t, k) = logE(k, Y(n, t)) + max(delta_probs(t-1, :)' + logA(:, k));
+             [maxP, maxZ] = max(delta_probs(t-1, :)' + logA(:, k));
+             Z(t, k) = maxZ;
+             delta_probs(t, k) = logE(k, Y(n, t)) + maxP';
          end
-         % keep track of max porob latent
-         [nothing, S_e(n, t)] = max(delta_probs(t, :));
-    end 
+    end
+    % backtrack
+    [nothing, S_e(n, T)] = max(delta_probs(T, :));
+    for t=T-1:-1:1
+          S_e(n, t) = Z(t+1, S_e(n, t+1));
+    end
 end
 end
 
